c45be34ef43e52a46340a93736246a51b10ff82b
Souritz <souritz@gmail.com>
2019-01-14 04:10:34 +0000

    [Finish]イラスト投稿画面とユーザー編集画面のコーディングを完了させた

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -4,9 +4,14 @@ class IllustrationsController < ApplicationController
   end
 
   def create
+    @illustration = Illustration.new(illustration_params)
+    @illustration.save
+    redirect_to illustration_path(illustration.id)
   end
 
   def show
+    @illustration = Illustration.find(params[:id])
+    @correction = Correction.new
   end
 
   def edit
@@ -19,5 +24,10 @@ class IllustrationsController < ApplicationController
   end
 
   def index
+  end
+
+  private
+  def illustration_params
+    params.require(:illustration).permit(:llust_image, :illust_title, :illust_introduction, :evaluation_point)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_mypage_path(params[:id])
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :history_year, :history_month, :introduction_text, :user_image)
   end
 end
