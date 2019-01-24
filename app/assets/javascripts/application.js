b05aa169ad09d29a05d4fb45e5d6db6d996a7f3d
Souritz <souritz@gmail.com>
2019-01-24 09:59:37 +0000

    [Update]タブメニューのタブの色が正常に切り替わるようにした

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,28 +26,30 @@
 //     });
 // });
 
-// マイページのタブメニュー
-$(function() {
-  // 要素(タブ)クリック時に処理させる
-  $('.tab-menu li').click(function() {
-    // クリックされた要素の順番を取得
-    var index = $('.tab-menu li').index(this);
-    // 以前に選択していた要素のactiveクラスを削除
-    $('.tab-menu li').removeClass('active');
-    // 新しくクリックした要素へactiveクラスを追加
-    $('this').addClass('active');
-    // 以前に選択していた要素を非表示・新しくクリックした要素のコンテンツを表示
-    $('.tab-menu-contents section').removeClass('tab-menu-show').eq(index).addClass('tab-menu-show');
+document.addEventListener("turbolinks:load", function(){
+  // マイページのタブメニュー
+  $(function() {
+    // 要素(タブ)クリック時に処理させる
+    $('.tab-menu li').click(function() {
+      // クリックされた要素の順番を取得
+      var index = $('.tab-menu li').index(this);
+      // 以前に選択していた要素のactiveクラスを削除
+      $('.tab-menu li').removeClass('active');
+      // 新しくクリックした要素へactiveクラスを追加
+      $(this).addClass('active');
+      // 以前に選択していた要素を非表示・新しくクリックした要素のコンテンツを表示
+      $('.tab-menu-contents section').removeClass('tab-menu-show').eq(index).addClass('tab-menu-show');
+    });
   });
-});
 
-// お気に入り機能 Ajaxによる非同期通信(ページ全体ではなくお気に入り機能だけ更新させる)
-$(function() {
-  $(document).on("ajax:success", ".like", function(e) {
-    if ($('#' + e.detail[0]).hasClass('fa-heart')){
-      $('#' + e.detail[0]).removeClass('fa-heart').addClass('fa-heart-o');
-    } else {
-      $('#' + e.detail[0]).removeClass('fa-heart-o').addClass('fa-heart');
-    }
+  // お気に入り機能 Ajaxによる非同期通信(ページ全体ではなくお気に入り機能だけ更新させる)
+  $(function() {
+    $(document).on("ajax:success", ".like", function(e) {
+      if ($('#' + e.detail[0]).hasClass('fa-heart')){
+        $('#' + e.detail[0]).removeClass('fa-heart').addClass('fa-heart-o');
+      } else {
+        $('#' + e.detail[0]).removeClass('fa-heart-o').addClass('fa-heart');
+      }
+    })
   })
-})
+});
